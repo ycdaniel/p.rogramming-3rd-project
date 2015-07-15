acce14b41b88cce21a0abf8b94a6500145fa650b
Chinseok Lee <allieuslee@gmail.com>
2015-07-16 01:23:28 +0900

    blog.views.index 뷰를 개선하여, Post 모델의 내용을 보여주도록 함.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
 
 
 def index(request):
-    return render(request, "blog/index.html", {})
+    post_list = Post.objects.all()
+    return render(request, "blog/index.html", {
+        'post_list': post_list,
+    })
+
