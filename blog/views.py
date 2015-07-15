81a08432f34609170902545aa17f5cab7d04e46e
Chinseok Lee <allieuslee@gmail.com>
2015-07-16 01:28:05 +0900

    Post detail 뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,10 @@ def index(request):
         'post_list': post_list,
     })
 
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, "blog/post_detail.html", {
+        'post': post,
+    })
+
