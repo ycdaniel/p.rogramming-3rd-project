45d00c78e9d11b28adf603b100fa7fe49b064756
Chinseok Lee <allieuslee@gmail.com>
2015-07-23 03:14:29 +0900

    media 파일 지원 설정

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    image = models.ImageField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
