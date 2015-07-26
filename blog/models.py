d3051d586a253ff0dec5e005bbbf401e463e41c3
Chinseok Lee <allieuslee@gmail.com>
2015-07-26 20:11:15 +0900

    ImageField 후처리 지원 (썸네일, 사각형자르기)

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,7 @@
+from django.core.files import File
 from django.db import models
+from django.db.models.signals import pre_save
+from blog.utils import square_image, thumbnail
 
 
 class Post(models.Model):
@@ -7,3 +10,15 @@ class Post(models.Model):
     image = models.ImageField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+def pre_on_post_save(sender, **kwargs):
+    post = kwargs['instance']
+    if post.image:
+        max_width = 300
+        if post.image.width > max_width or post.image.height > max_width:
+            processed_file = thumbnail(post.image.file, max_width, max_width)
+            # processed_file = square_image(post.image.file, max_width)
+            post.image.save(post.image.name, File(processed_file))
+
+pre_save.connect(pre_on_post_save, sender=Post)
