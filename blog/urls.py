beb6f5fab89b3e6a8663734e3ab3f468b0fdb544
Chinseok Lee <allieuslee@gmail.com>
2015-08-03 15:17:31 +0900

    html form, QueryDict 을 통한 Post 생성

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', 'blog.views.index'),
+    url(r'^new/$', 'blog.views.new'),
     url(r'^(?P<pk>\d+)/$', 'blog.views.detail'),
     url(r'^about/$', 'blog.views.about'),
     url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', 'blog.views.sum_xy'),
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseBadRequest
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.csrf import csrf_exempt
 from blog.models import Post
 
 
@@ -9,8 +11,40 @@ def index(request):
     })
 
 
+@csrf_exempt
+def new(request):
+    print("---- new ----")
+    print("request.GET = {}".format(request.GET))
+    print("request.POST = {}".format(request.POST))
+    print("request.FILES = {}".format(request.FILES))
+
+    if request.method == 'POST':
+        if 'title' not in request.POST:
+            return HttpResponseBadRequest('제목을 지정해주세요.')
+        if not request.POST['title']:
+            return HttpResponseBadRequest('제목이 비었어요.')
+
+        if 'content' not in request.POST:
+            return HttpResponseBadRequest('내용을 지정해주세요.')
+        if not request.POST['content']:
+            return HttpResponseBadRequest('내용이 비었어요.')
+
+        title = request.POST['title']
+        content = request.POST['content']
+        post = Post(title=title, content=content)
+        post.save()
+
+    return render(request, "blog/form.html")
+
+
 def detail(request, pk):
+#   try:
+#       post = Post.objects.get(pk=pk)
+#   except Post.DoesNotExist:
+#       raise Http404
+
     post = get_object_or_404(Post, pk=pk)
+
     return render(request, "blog/post_detail.html", {
         'post': post,
     })
